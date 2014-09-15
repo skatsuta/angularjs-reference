5b40c21646224b8979a8fdf0fe073eba0b6ea017
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-09-15 16:25:56 +0900

    Section 2-1-1 to 2-1-16

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var MyController = function($scope) {
   $scope.message = 'Hello, World!';
   $scope.action = function() {
     $scope.message = 'Goodbye, Everyone!';
   };
 };
-var appModule = angular.module('app', []);
+var appModule = angular.module('app', ['ngSanitize']);
 appModule.controller('myController', MyController);
