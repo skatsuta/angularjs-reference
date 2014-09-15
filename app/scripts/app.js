1666efcb6c1418d99b4b6b3b74db2de4acf5fb7e
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-09-15 16:55:01 +0900

    Add ngChange sample

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+(function() { "use strict";
 
 var MyController = function($scope) {
   $scope.message = 'Hello, World!';
@@ -6,5 +6,23 @@ var MyController = function($scope) {
     $scope.message = 'Goodbye, Everyone!';
   };
 };
+
 var appModule = angular.module('app', ['ngSanitize']);
+
 appModule.controller('myController', MyController);
+
+appModule.controller('CountCtrl', ['$scope', function ($scope) {
+  var maxCount = 140;
+  $scope.count = maxCount;
+  $scope.change = function() {
+    $scope.count = maxCount - $scope.tweet.length;
+    if ($scope.count < 0) {
+      $scope.count = Math.abs($scope.count);
+      $scope.isOver = true;
+    } else {
+      $scope.isOver = false;
+    }
+  };
+}]);
+
+})();
